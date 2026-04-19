547437d391c4646bc90459a801122915dffc6180
intelligentes <intelligentes@icloud.com>
2026-04-19 21:26:39 +0300

    Добавил флейки niri и helium, добавил kitty, helix, waybar

--- flake.nix
+++ flake.nix
@@ -2,23 +2,37 @@
   decription = "йоу"
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    niri = {
+      url = "github:sodiboo/niri-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    helium = {
+      url = "github:schembriaiden/helium-browser-nix-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }: {
+  outputs = { nixpkgs, home-manager, niri, helium, ... }: {
     nixosConfigurations.ideapad = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         .configuration.nix
+        niri.nixosModules.niri
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.int = import ./home.nix;
+          home-manager.sharedModules = [ niri.homeModules.niri ];
+          home-manager.extraSpecialArgs = { inherit helium; };
         }
       ];
     };
--- home.nix
+++ home.nix
@@ -1,6 +1,24 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, helium ... }: {
   # ===== ОСНОВНОЕ =====
   home.username = "int";
   home.homeDirectory = "/home/int";
+
+  # ===== ПРИЛОЖЕНИЯ =====
+  home.packages = with pkgs; [
+    helium.packages.x86_64-linux.default  # Helium browser
+  ];
+
+  # ===== NIRI =====
+  programs.niri.enable = true;
+
+  # ===== KITTY =====
+  programs.kitty.enable = true;
+
+  # ===== HELIX =====
+  programs.helix.enable = true;
+
+  # ===== WAYBAR =====
+  programs.waybar.enable = true;
+
   home.stateVersion = "25.05";
 }
