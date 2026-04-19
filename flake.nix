1dbb6ef40935477d6625ab564dd0b4a1b21b27af
intelligentes <intelligentes@icloud.com>
2026-04-19 21:08:32 +0300

    Добавил минимальные home-manager и flakes

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  decription = "йоу"
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        .configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.int = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,6 @@
+{ config, pkgs, ... }: {
+  # ===== ОСНОВНОЕ =====
+  home.username = "int";
+  home.homeDirectory = "/home/int";
+  home.stateVersion = "25.05";
+}
